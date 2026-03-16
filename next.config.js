e68cdd3c9b1fd87279deee29264311dc0c45119a
ghloctane <239655473+ghloctane@users.noreply.github.com>
2026-03-17 01:52:20 +0500

    update

--- next.config.js
+++ next.config.js
@@ -20,7 +20,6 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 31536000, // 3. Caching Domain level for Next/Image proxy
-    quality: 75,
     remotePatterns: [
       { protocol: 'https', hostname: 'storage.googleapis.com' },
       { protocol: 'https', hostname: 'images.unsplash.com' },
